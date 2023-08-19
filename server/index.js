86e87de014ae21f0f0176edb759ced39ee0cfe3a
Ansh Mourya <anshmourya657@gmail.com>
2023-08-19 11:15:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors({
 }))
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URl);
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true'); // Set the header to 'true'
@@ -32,5 +32,5 @@ app.use(visit)
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(process.env.CLIENT_URL))
 app.listen(port, () => console.log(`SpineHealth app listening on port ${process.env.port || port}!`)) 
